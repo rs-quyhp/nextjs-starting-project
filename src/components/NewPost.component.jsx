8b40ce0465cfd91684bf2f1723fb8ca51d527be1
rs-quyhp <quyhp@runsystem.net>
2025-07-06 20:42:37 +0700

    24-27. states

---
+++ src/components/NewPost.component.jsx
@@ -0,0 +1,19 @@
+import classes from './NewPost.module.css';
+
+function NewPost(props) {
+  const {setName, setContent} = props
+  return (
+    <form className={classes.form}>
+      <p>
+        <label htmlFor="body">Text</label>
+        <textarea id="body" required rows={3} onChange={setName}/>
+      </p>
+      <p>
+        <label htmlFor="name">Your name</label>
+        <input type="text" id="name" required onChange={setContent} />
+      </p>
+    </form>
+  );
+}
+
+export default NewPost;
--- src/components/PostsList.component.jsx
+++ src/components/PostsList.component.jsx
@@ -1,17 +1,33 @@
 import Post from "./Post.component"
+import NewPost from "./NewPost.component"
 import styles from "./PostsList.module.css";
+import { useState } from "react";
 
 const PostsList = (props) => {
-const {data} = props
+    const {data} = props
+    const [name, setName] = useState('SBH')
+    const [content, setContent] = useState('Content test!')
+
+    const onNameChanged = (event) => {
+        setName(event.target.value)
+    }
+    
+    const onContentChanged = (event) => {
+        setContent(event.target.value)
+    }
 
     return (
-    <ul className={styles.posts}>
-      {data.map((item) => (
-        <li>
-            <Post name={item.name} content={item.content} />
-        </li>
-      ))}
-    </ul>
+        <>
+            <NewPost setName={onNameChanged} setContent={onContentChanged}/>
+            <ul className={styles.posts}>
+                <li><Post name={name} content={content}/></li>
+            {data.map((item) => (
+                <li>
+                    <Post name={item.name} content={item.content} />
+                </li>
+            ))}
+            </ul>
+        </>
     );
 }
 
